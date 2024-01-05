9082dd67003ee64112c22422a897fb9bed466c2c
mashhoodalikhan <155739062+mashhoodalikhan@users.noreply.github.com>
2024-01-05 07:18:40 -0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,9 @@
+
+b = int(input("Input the base : "))
+h = int(input("Input the height : "))
+
+# Calculate the area of the triangle using the formula: (base * height) / 2.
+area = b * h / 2
+
+# Print the calculated area of the triangle.
+print("area = ", area)
