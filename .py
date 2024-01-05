d3ede88c4aa0d08915b35bcc13de9c07ff0b4dac
mashhoodalikhan <155739062+mashhoodalikhan@users.noreply.github.com>
2024-01-05 07:15:34 -0800

    Add files via upload

---
+++ .py
@@ -0,0 +1,7 @@
+num1= float(input("Enter the first floating-point number:"))
+num2= float(input("Enter the second floating-point number:"))
+#THE FORMULA OF AVERAGE:(num1+num2)/2
+Average=(num1+num2)/2
+print(f"The average is:{Average}:")
+
+
---
+++ python assigment 1.py
@@ -0,0 +1,5 @@
+radius= float(input("Enter the radius of sphere:"))
+volume=(4/3)*3.142*(radius**3) 
+surfacearea=4*3.142*(radius**2)
+print (f"The volume of sphere is:{volume:}")
+print(f"the surface area of sphere is:{surfacearea}")
